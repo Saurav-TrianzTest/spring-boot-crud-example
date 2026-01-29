bb14c256eeb851064a7c1747022bc3fdee27af6f
Modernize Studio <studio@modernize.com>
2026-01-29 11:11:51 +0000

    Commit for planId: 1048

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.javatechie.crud.example.controller;
 
 import com.javatechie.crud.example.entity.Product;
 import com.javatechie.crud.example.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,8 +9,11 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
 
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
--- src/main/java/com/javatechie/crud/example/service/ProductService.java
+++ src/main/java/com/javatechie/crud/example/service/ProductService.java
@@ -2,15 +2,17 @@ package com.javatechie.crud.example.service;
 
 import com.javatechie.crud.example.entity.Product;
 import com.javatechie.crud.example.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
-    @Autowired
-    private ProductRepository repository;
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
 
     public Product saveProduct(Product product) {
         return repository.save(product);
@@ -39,10 +41,13 @@ public class ProductService {
 
     public Product updateProduct(Product product) {
         Product existingProduct = repository.findById(product.getId()).orElse(null);
-        existingProduct.setName(product.getName());
-        existingProduct.setQuantity(product.getQuantity());
-        existingProduct.setPrice(product.getPrice());
-        return repository.save(existingProduct);
+        if (existingProduct != null) {
+            existingProduct.setName(product.getName());
+            existingProduct.setQuantity(product.getQuantity());
+            existingProduct.setPrice(product.getPrice());
+            return repository.save(existingProduct);
+        }
+        return null;
     }
 
 
